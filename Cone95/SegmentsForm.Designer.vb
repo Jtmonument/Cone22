39b41eb7856bfea62690f71539ab5e99c2fb3eb8
Jonathan Taylor <jtaylor@processbarron.com>
2022-08-19 09:51:23 -0500

    Fixed SegmentsForm. Closes #2

--- Cone95/SegmentsForm.Designer.vb
+++ Cone95/SegmentsForm.Designer.vb
@@ -24,34 +24,50 @@ Partial Class SegmentsForm
     Private Sub InitializeComponent()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(60, 66)
+        Me.TextBox1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TextBox1.Location = New System.Drawing.Point(61, 66)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(157, 23)
+        Me.TextBox1.Size = New System.Drawing.Size(157, 29)
         Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "2"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Label1.Location = New System.Drawing.Point(39, 29)
+        Me.Label1.Location = New System.Drawing.Point(37, 31)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(202, 21)
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "Enter Number Of Segments"
         '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.Location = New System.Drawing.Point(90, 103)
+        Me.Button1.MinimumSize = New System.Drawing.Size(100, 25)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 28)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Ok"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'SegmentsForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(290, 118)
+        Me.ClientSize = New System.Drawing.Size(290, 143)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TextBox1)
         Me.Name = "SegmentsForm"
-        Me.Text = "SegmentsForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -59,4 +75,5 @@ Partial Class SegmentsForm
 
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
 End Class
--- Cone95/SegmentsForm.vb
+++ Cone95/SegmentsForm.vb
@@ -1,3 +1,15 @@
 ﻿Public Class SegmentsForm
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim temp As Integer
+        If Integer.TryParse(TextBox1.Text, temp) Then
+            Close()
+        End If
+    End Sub
 
+    Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+        Dim temp As Integer
+        If Char.IsWhiteSpace(e.KeyChar) AndAlso Integer.TryParse(TextBox1.Text, temp) Then
+            Close()
+        End If
+    End Sub
 End Class
