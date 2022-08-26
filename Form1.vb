28dab3cd7fd1cdf4031705f03befd980adf7e619
Jonathan Taylor <jtaylor@processbarron.com>
2022-08-26 09:27:45 -0500

    Remove unnecessary empty event

--- Form1.vb
+++ Form1.vb
@@ -5,10 +5,6 @@ Imports System.Text
 
 Public Class Form1
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
         '
